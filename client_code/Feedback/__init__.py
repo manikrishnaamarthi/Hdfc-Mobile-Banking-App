03532127e41420b627830e6c382d786c15a8f19b
maniamarthi@gmail.com <maniamarthi@gmail.com>
2023-10-11 19:25:08 +0000

    Created ServerModule1 and Feedback; Edited settings; Deleted Form1

---
+++ client_code/Feedback/__init__.py
@@ -0,0 +1,32 @@
+from ._anvil_designer import FeedbackTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Feedback(FeedbackTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+   name = self.name_box.text
+   email = self.email_box.text
+   password = self.password_box.text
+
+   anvil.server.call('add_feedback', name, email, password)
+   Notification("Details Submitted!").show()
+   self.clear_inputs()
+
+  
+  def clear_inputs(self):
+    self.name_box.text=""
+    self.email_box.text=""
+    self.password_box.text=""
+    
+   
+
+  
